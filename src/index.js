9ed2e64b155759799413c8ebcfeaba42f7c7237c
calvis onyango <calvisonyango265@gmail.com>
2025-03-15 13:54:26 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const input = document.querySelector("input").value;
+    const list = document.querySelector("#tasks"); 
+  
+    if (input.trim() !== "") { 
+      const newTask = document.createElement("li");
+      newTask.textContent = input; 
+      list.appendChild(newTask); 
+    }
+  });
+  
 });
