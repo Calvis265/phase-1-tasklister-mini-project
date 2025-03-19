ee01ed6c9333f289564741d42a6ce8d1488d7ec7
calvis onyango <calvisonyango265@gmail.com>
2025-03-19 11:17:06 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
   document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
   
